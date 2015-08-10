642f9b93f53562d38ef2c87a98ed48d498226543
Triara <triara.xiii@gmail.com>
2015-08-10 16:59:28 -0700

    added circle ci yml file

--- src/savePerson.js
+++ src/savePerson.js
@@ -2,7 +2,7 @@
 
 const neo4j = require('neo4j');
 
-const db = new neo4j.GraphDatabase('http://neo4j:pass@localhost:7474');
+const db = new neo4j.GraphDatabase('http://neo4j:neo4j@localhost:7474');
 
 
 module.exports = dataToSave => {
